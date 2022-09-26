fcb0572a1db7257c7b7b7f251fe8e512a64954af
mirek <kol_mirek@o2.pl>
2022-09-26 21:16:42 +0200

    26-09-2022 - Poprawa funkcjonalności. Serwisant wstrzymuje i przywraca zlecenie. Dodatkowo serwisant ma możliwość zakładania zleceń jednak ich zatwierdzenie wykonuje nadzór.

--- projekt/settings.py
+++ projekt/settings.py
@@ -53,11 +53,11 @@ INSTALLED_APPS = [
 
 #Email dom
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'plmko@rimaster.com'
-EMAIL_HOST_PASSWORD = 'Kmirek0326'
+EMAIL_HOST_PASSWORD = 'M1r3kol'
 EMAIL_RECIVE_USER = 'mirek.kolczynski@gmail.com' #adres serwisantów
 
 MIDDLEWARE = [
